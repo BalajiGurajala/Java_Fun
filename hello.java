09d710264a014e9822c2d4855da2cabb1e475503
balaji <bp.gurajala@outlook.com>
2024-02-17 11:46:55 +0530

    my first com

---
+++ hello.java
@@ -0,0 +1,18 @@
+class st {
+public static void main(String a[]){
+
+           StringBuffer st =new StringBuffer("Balaji");
+           
+           //st.replace(1, 2, "p"); // To replace the value by positions
+
+           //st.append(" Prasad");  // To add the concte string value
+
+           st.capacity();
+
+           
+           System.out.println(st);
+
+}
+
+
+}
